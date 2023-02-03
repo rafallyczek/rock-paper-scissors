ccc8f59c18791cd63133af96bdd0a389cb6a4ac6
rafallyczek <rafal.lyczek@gmail.com>
2023-02-03 13:26:29 +0100

    Write code for game
    Wrote code for rock paper scissors game which works in console.

---
+++ script.js
@@ -0,0 +1,100 @@
+// 0 equals Rock
+// 1 equals Paper
+// 2 equals Scissors
+
+let playerScore;
+let computerScore;
+
+function getComputerChoice(){
+
+    return Math.floor(Math.random()*3);
+
+}
+function getPlayerChoice(){
+
+    let playerChoice = prompt("What is your choice?");
+    playerChoice = playerChoice.toLowerCase();
+
+    if(playerChoice=="rock"){
+        return 0;
+    }else if(playerChoice=="paper"){
+        return 1;
+    }else if(playerChoice=="scissors"){
+        return 2;
+    }else{
+        return -1;
+    }
+
+}
+function playRound(playerChoice,computerChoice){
+
+    if(playerChoice==0){
+        if(computerChoice==1){
+            console.log("%cYou Lose! Paper beats Rock.","color: red");
+            addPoints(0);
+        }else if(computerChoice==2){
+            console.log("%cYou Win! Scissors beats Rock.","color: green");
+            addPoints(1);
+        }else{
+            console.log("%cIt's a tie!","color: goldenrod");
+        }
+    }else if(playerChoice==1){
+        if(computerChoice==0){
+            console.log("%cYou Win! Paper beats Rock.","color: green");
+            addPoints(1);
+        }else if(computerChoice==2){
+            console.log("%cYou Lose! Scissors beats Paper.","color: red");
+            addPoints(0);
+        }else{
+            console.log("%cIt's a tie!","color: goldenrod");
+        }
+    }else if(playerChoice==2){
+        if(computerChoice==0){
+            console.log("%cYou Lose! Rock beats Scissors.","color: red");
+            addPoints(0);
+        }else if(computerChoice==1){
+            console.log("%cYou Win! Scissors beats Paper.","color: green");
+            addPoints(1);
+        }else{
+            console.log("%cIt's a tie!","color: goldenrod");
+        }
+    }else{
+        console.log("Invalid input! No point's added.");
+    }
+
+}
+// 0 equals computer-win
+// 1 equals player-win
+function addPoints(winner){
+
+    if(winner==0){
+        computerScore++;
+    }else{
+        playerScore++;
+    }
+
+}
+function game(){
+
+    playerScore = 0;
+    computerScore = 0;
+
+    for(let i=0;i<5;i++){
+        playRound(getPlayerChoice(),getComputerChoice());
+        if(i!=4){
+            console.log(`Current score is: player ${playerScore}:${computerScore} computer`);
+        }
+    }
+
+    console.log(`Final score is: player ${playerScore}:${computerScore} computer`);
+
+    if(playerScore>computerScore){
+        console.log("%cYou win!","background: green; color: black");
+    }else if(playerScore<computerScore){
+        console.log("%cYou lose!","background: red; color: black");
+    }else{
+        console.log("%cIt's a tie!","background: goldenrod; color: black");
+    }
+
+}
+game();
